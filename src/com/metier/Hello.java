2ed4417c2a491fdfde1298e2ec2c74f40faab923
eleye <elyeleye@gmail.com>
2019-11-10 17:31:55 +0100

    ajout

---
+++ src/com/metier/Hello.java
@@ -0,0 +1,16 @@
+package com.metier;
+
+import com.modele.Student;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Student yo = new Student();
+		yo.setName("yo");
+		yo.setId(4);
+		System.out.println("Hello boy " + yo.getName());
+			
+
+	}
+
+}

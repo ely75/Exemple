3c82f805cce637a46394a9a4091c82f1949d3357
= <elyeleye@gmail.com>
2021-07-13 11:54:09 +0200

    commit

--- src/com/metier/Hello.java
+++ src/com/metier/Hello.java
@@ -7,8 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		Student yo = new Student();
 		yo.setName("yo"); 
-		yo.setId(4);
-		
+		yo.setId(4);		
 		System.out.println("Hello boy " + yo.getName() + "  " + yo.getId());
 	}
 }

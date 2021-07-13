89d02ec422a665de534d180a0caf681c86fe08ee
= <elyeleye@gmail.com>
2021-07-13 12:14:08 +0200

    another commit

--- src/com/metier/Hello.java
+++ src/com/metier/Hello.java
@@ -7,7 +7,8 @@ public class Hello {
 	public static void main(String[] args) {
 		Student yo = new Student();
 		yo.setName("yo"); 
-		yo.setId(4);		
+		yo.setId(4);	
+		System.out.println("***********************");
 		System.out.println("Hello boy " + yo.getName() + "  " + yo.getId());
 	}
 }
